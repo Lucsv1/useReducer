7997c0ff600af899b19fe15452028d8ba8fdbd27
Lucas <lcasgalindo@gmail.com>
2023-10-24 18:07:24 -0300

    usando useReducer com mais dados

--- App.js
+++ App.js
@@ -1,101 +1,86 @@
 import { StatusBar } from "expo-status-bar";
 import {
   Button,
-  SafeAreaView,
+  FlatList,
   StyleSheet,
   Text,
   TextInput,
   View,
 } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useReducer, useState, useContext } from "react";
-import { object, string } from "yup";
+import { useReducer, useState } from "react";
 
 const Stack = createNativeStackNavigator();
 
-
+const productReducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return {
+        ...state,
+        objetos: { nome: "", quantidade: "", preco: "" },
+      };
+    case "remover":
+      return {
+        ...state,
+        objetos: { nome: "", quantidade: "", preco: "" },
+      };
+    default:
+      return state;
+  }
+};
 
 const Fomulario = () => {
-  const schema = object({
-    nome: string()
-      .required("O campo é necessario")
-      .min(5, "O nome precisa de pelo menos 5 caracteres")
-      .max(10, "O nome deve conter até 10 caracteres  "),
-    quantidade: string()
-      .required("O campo é obrigatorio")
-      .min(10, "é necessario pelo menos 10 caracteres")
-      .max(15, "o maximo de caracteres é 15"),
-    preco: string()
-      .required("O campo é obrigatorio")
-      .min(3, "é necessario 3 caracters")
-      .max(10, "o maximo de caracteres é 10"),
-  });
-
-  const [nome, setNome] = useState("");
-  const [nomeErro, setNomeErro] = useState("");
-  const [quantidade, setQuantidade] = useState("");
-  const [quantidadeErro, setQuantidadeErro] = useState("");
-  const [preco, setPreco] = useState("");
-  const [precoErro, setPrecoErro] = useState("")
-
-  const productReducer = (state, action) => {
-    switch (action.type) {
-      case "add":
-        return { ...state, nome: action.payload };
-      default:
-        return state;
-    }
+  const Lista = () => {
+    <View></View>;
   };
 
-  const [state, dispatch] = useReducer(productReducer, { nome: "" });
+  const lista = [];
 
-  const MudarName = (e) => {
-    dispatch({ type: "add", payload: e });
+  const objetos = {
+    nome: "",
+    quantidade: "",
+    preco: "",
+  };
+  const [state, dispatch] = useReducer(productReducer, objetos);
+
+  const Registrar = () => {
+    dispatch({ type: "add", payload: objetos });
+  };
+
+  const Remover = () => {
+    dispatch({ type: "remover" });
   };
 
   return (
-    <SafeAreaView>
+    <View>
       <View style={styles.container}>
         <Text>Nome</Text>
-        <TextInput value={nome} onChangeText={setNome} />
-        <Text style={{ color: "red" }}>{nomeErro}</Text>
-        <Text>Quantidade</Text>
-        <TextInput value={quantidade} onChangeText={setQuantidade} />
-        <Text>{quantidadeErro}</Text>
-        <Text>Preço</Text>
-        <TextInput value={preco} onChangeText={setPreco} />
-        <Text>{precoErro}</Text>
-        <Button
-          title="adicionar"
-          onPress={() => {
-            schema
-              .validate({ nome, quantidade, preco }, { abortEarly: false })
-              .then(() => {
-                setNomeErro("");
-                setQuantidadeErro("");
-                setPrecoErro("")
-              })
-              .catch((err) => {
-                setNomeErro("");
-                setQuantidadeErro("");
-                setPrecoErro("");
-                err.inner.forEach((erro) => {
-                  if (erro.path === "nome") {
-                    setNomeErro(erro.message);
-                  } else if (erro.path === "quantidade") {
-                    setQuantidadeErro(erro.message);
-                  } else if (erro.path === "preco"){
-                    setPrecoErro(erro.message)
-                  }
-                });
-              });
+        <TextInput
+          onChangeText={(txt) => {
+            state.nome = txt;
           }}
         />
+        <Text>Quantidade</Text>
+        <TextInput
+          onChangeText={(txt) => {
+            state.quantidade = txt;
+          }}
+        />
+        <Text>Preço</Text>
+        <TextInput
+          onChangeText={(txt) => {
+            state.preco = txt;
+          }}
+        />
+        <Button title="adicionar" onPress={Registrar} />
+        <Button title="Remover" onPress={Remover} />
       </View>
-      <View>
-        <Text>Nome:</Text>
+      <View style={{ flex: 10 }}>
+        <Text>Nome:{state.nome}</Text>
+        <Text>Quantidade: {state.quantidade}</Text>
+        <Text>Preço: {state.preco}</Text>
       </View>
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -109,7 +94,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 100,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
--- Verificar.js
+++ Verificar.js
@@ -1,25 +1,111 @@
-import { useReducer, useState, useContext } from "react";
-import { number, object, string } from "yup";
+// loja.js
+import { createStore } from 'redux';
 
-export const Verificar = () => {
-  schema.validate({nome, qauntidade, preco})
-  .then(()=>{
-    setNomeErro("")
-    setQuantidadeErro("")
-    setPrecoErro("")
-  }).catch((err)=>{
-    setNomeErro("")
-    setQuantidadeErro("")
-    setPrecoErro("")
-    err.inner.forEach((err) => {
-        if(err.path === "nome"){
-            setNomeErro(err.message)
-        } else if(err.path === "quantidade"){
-            setQuantidadeErro(err.message)
-        } else if(err.path === "preco"){
-            setPrecoErro(err.message)
-        }
-        
-    });
-  })
+const estadoInicial = {
+  produtos: [],
+};
+
+const redutorDeProdutos = (estado = estadoInicial, acao) => {
+  switch (acao.type) {
+    case 'ADICIONAR_PRODUTO':
+      return {
+        ...estado,
+        produtos: [...estado.produtos, acao.payload],
+      };
+    case 'ATUALIZAR_PRODUTO':
+      const produtosAtualizados = estado.produtos.map((produto) =>
+        produto.id === acao.payload.id ? acao.payload : produto
+      );
+      return {
+        ...estado,
+        produtos: produtosAtualizados,
+      };
+    case 'EXCLUIR_PRODUTO':
+      const produtosFiltrados = estado.produtos.filter(
+        (produto) => produto.id !== acao.payload
+      );
+      return {
+        ...estado,
+        produtos: produtosFiltrados,
+      };
+    default:
+      return estado;
+  }
 };  
+
+export const loja = createStore(redutorDeProdutos);
+
+// AppProdutos.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
+
+const AppProdutos = () => {
+  const produtos = useSelector((state) => state.produtos);
+  const dispatch = useDispatch();
+
+  const [nome, setNome] = useState('');
+  const [quantidade, setQuantidade] = useState('');
+  const [preco, setPreco] = useState('');
+
+  const adicionarProduto = () => {
+    // Realize a validação dos campos aqui, se necessário
+    if (nome && quantidade && preco) {
+      const novoProduto = {
+        id: Date.now(),
+        nome,
+        quantidade,
+        preco,
+      };
+      dispatch({ type: 'ADICIONAR_PRODUTO', payload: novoProduto });
+      setNome('');
+      setQuantidade('');
+      setPreco('');
+    }
+  };
+
+  const excluirProduto = (id) => {
+    dispatch({ type: 'EXCLUIR_PRODUTO', payload: id });
+  };
+
+  const editarProduto = (produto) => {
+    // Implemente a edição do produto aqui, se necessário
+  };
+
+  return (
+    <View>
+      <Text>Produto</Text>
+      <TextInput
+        placeholder="Nome"
+        value={nome}
+        onChangeText={(text) => setNome(text)}
+      />
+      <TextInput
+        placeholder="Quantidade"
+        value={quantidade}
+        onChangeText={(text) => setQuantidade(text)}
+      />
+      <TextInput
+        placeholder="Preço"
+        value={preco}
+        onChangeText={(text) => setPreco(text)}
+      />
+      <Button title="Adicionar" onPress={adicionarProduto} />
+      <FlatList
+        data={produtos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.nome}</Text>
+            <Text>Quantidade: {item.quantidade}</Text>
+            <Text>Preço: {item.preco}</Text>
+            <Button title="Editar" onPress={() => editarProduto(item)} />
+            <Button title="Excluir" onPress={() => excluirProduto(item.id)} />
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default AppProdutos;
