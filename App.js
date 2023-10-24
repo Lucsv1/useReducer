30c7cc4f421af30c116e88846c41a5bfe1d0570f
Lucas <lcasgalindo@gmail.com>
2023-10-23 22:46:17 -0300

    useReducer

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useReducer, useState } from 'react';
+
+const Stack = createNativeStackNavigator();
+
+
+
+
+
+const Fomulario = () =>{
+
+  const productReducer = (state, action) =>{
+    switch (action.type){
+      case 'add':
+        return {...state, nome: action.payload};
+      default:
+        return state
+    }
+  }
+
+
+  const [state, dispatch] = useReducer(productReducer, {nome: ''})
+
+  const MudarName = (e) =>{
+    dispatch({type: 'add', payload: e })
+  }
+  
+
+  return(<View >
+    <View style={styles.container}>
+      <Text>Nome</Text>
+      <TextInput value={state.nome} onChangeText={MudarName}/>
+      <Text>Quantidade</Text>
+      <TextInput/>
+      <Text>Preço</Text>
+      <TextInput/>
+      <Button title='adicionar' onPress={productReducer}/>
+    </View>
+    <View>
+      <Text>Nome:{state.nome}</Text>
+    </View>
+  </View>)
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Fomulario/>
     </View>
   );
 }
